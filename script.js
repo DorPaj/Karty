7b9e4f61a83d25df98281b76ab1804d8c928996d
DorPaj <100585183+DorPaj@users.noreply.github.com>
2023-09-11 11:11:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let maxZindex = 5
+
+function as() {
+	maxZindex += 1
+	document.getElementById("as").style.zIndex = maxZindex
+}
+
+function krol() {
+	maxZindex += 1
+	document.getElementById("krol").style.zIndex = maxZindex
+}
+
+function dama() {
+	maxZindex += 1
+	document.getElementById("dama").style.zIndex = maxZindex
+}
+
+function walet() {
+	maxZindex += 1
+	document.getElementById("walet").style.zIndex = maxZindex
+}
+
+function rew() {
+	maxZindex += 1
+	document.getElementById("rew").style.zIndex = maxZindex
+}
